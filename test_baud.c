1a801d11fb50380dfb6ef86dee9eaec1040a146c
Jinbuhm Kim <jinbuhm.kim@gmail.com>
2018-01-01 12:31:17 +0900

    update code
    fix the argc parameter

--- test_baud.c
+++ test_baud.c
@@ -56,7 +56,7 @@ int main(int argc, char* argv[])
     struct timeval tout;
     unsigned char send_ch, recv_ch;    
 
-    if (argc != 2) {
+    if (argc != 3) {
         printf("%s device speed\n\nSet speed for a serial device.\nFor instance:\n    %s /dev/ttyUSB0 250000\n", argv[0], argv[0]);                   
         return -1;
     }
